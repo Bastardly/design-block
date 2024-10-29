2283fab2363a12f626232046365b84a04edb174c
Flemming Knøss Hansen <fha@kmd.dk>
2024-10-29 14:45:24 +0100

    Insert component

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,118 @@
-import type * as Types from "./types";
-export type { Types };
-export { router } from "./router";
-export { RouterOutlet } from "./router-outlet";
-export { Redirect } from "./redirect";
-export { useInitRouter } from "./use-router";
-export { Link } from "./link";
+export { define } from "@ognaf/core";
+
+interface IContainerDefaults {
+  zero: string;
+  xs: string;
+  sm: string;
+  md: string;
+  lg: string;
+  xl: string;
+  xxl: string;
+}
+
+type IPaddings = "p" | "px" | "py" | "pt" | "pr" | "pb" | "pl";
+type IMargins = "m" | "mx" | "my" | "mt" | "mr" | "mb" | "ml";
+type IFlexSpacingAttributes = "rowgap" | "colgap";
+type ISizeAttribute = IPaddings | IMargins | IFlexSpacingAttributes;
+type IAttribute = ISizeAttribute | "direction";
+
+type ISizes = keyof IContainerDefaults;
+
+/**
+ * IContainerDiv is a type used to define props in React
+ */
+export type IContainerDiv = Partial<
+  | Record<ISizeAttribute, keyof IContainerDefaults> & {
+      direction: "row" | "column";
+    }
+>;
+
+export class ContainerDiv extends HTMLElement {
+  #transitionMs = 300;
+
+  constructor(transitionMs?: number) {
+    super();
+    this.#transitionMs = transitionMs || this.#transitionMs;
+  }
+
+  static sizeAttributes: ISizeAttribute[] = [
+    "p",
+    "px",
+    "py",
+    "pt",
+    "pr",
+    "pb",
+    "pl",
+    "m",
+    "mx",
+    "my",
+    "mt",
+    "mr",
+    "mb",
+    "ml",
+    "rowgap",
+    "colgap",
+  ];
+
+  static observedAttributes: IAttribute[] = [
+    ...ContainerDiv.sizeAttributes,
+    "direction",
+  ];
+
+  #sizes: ISizes[] = ["zero", "xs", "sm", "md", "lg", "xl", "xxl"];
+  #attrMap: Record<ISizeAttribute, (keyof CSSStyleDeclaration)[]> = {
+    p: ["padding"],
+    px: ["paddingLeft", "paddingRight"],
+    py: ["paddingTop", "paddingBottom"],
+    pt: ["paddingTop"],
+    pr: ["paddingRight"],
+    pb: ["paddingBottom"],
+    pl: ["paddingLeft"],
+    m: ["margin"],
+    mx: ["marginLeft", "marginRight"],
+    my: ["marginTop", "marginBottom"],
+    mt: ["marginTop"],
+    mr: ["marginRight"],
+    mb: ["marginBottom"],
+    ml: ["marginLeft"],
+    colgap: ["columnGap"],
+    rowgap: ["rowGap"],
+  };
+
+  defaults: IContainerDefaults = {
+    zero: "0",
+    xs: "var(--xs)",
+    sm: "var(--sm)",
+    md: "var(--md)",
+    lg: "var(--lg)",
+    xl: "var(--xl)",
+    xxl: "var(--xxl)",
+  };
+
+  updateAttributes() {
+    ContainerDiv.sizeAttributes.forEach((attr) => {
+      const size = this.getAttribute(attr) as ISizes | undefined;
+      if (size && this.#sizes.includes(size)) {
+        this.#attrMap[attr].forEach((element) => {
+          this.style[element as string] = this.defaults[size];
+        });
+      } else {
+        this.#attrMap[attr].forEach((element) => {
+          this.style[element as string] = undefined;
+        });
+      }
+    });
+    this.style.direction =
+      this.getAttribute("direction") === "row" ? "row" : "column";
+  }
+
+  connectedCallback() {
+    this.style.display = "flex";
+    this.style.transition = `all ${this.#transitionMs}ms`;
+    this.updateAttributes();
+  }
+
+  attributeChangedCallback() {
+    this.updateAttributes();
+  }
+}
